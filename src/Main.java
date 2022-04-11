0355b4a8bd0337a4cd95bb2517d301110522dc41
Dorian Sousa <calvo.dorian14@gmail.com>
2022-04-11 10:02:06 +0200

    Setup

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args){launch(args);}
-    
+
     public void start(Stage stage) throws Exception {
 
     }
