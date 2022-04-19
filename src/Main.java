b1dd3e0c2f43cd7660ddcba6c8ed89da38a41990
hrallil <mathias.larsen0@gmail.com>
2022-04-19 10:27:29 +0200

    added hello world window

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+
 
 public class Main extends Application {
-    public static void main(String[] args){launch(args);}
+    public static void main(String[] args){
+        launch(args);
+    }
 
     public void start(Stage stage) throws Exception {
-
+        Group root = new Group(); // the root is Group or Pane
+        Scene scene = new Scene(root, 500, 500, Color.RED);
+        stage.setTitle("JavaFX Demo");
+        stage.setScene(scene);
+        stage.show();
     }
 }
