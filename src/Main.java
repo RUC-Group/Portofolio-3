2c86098344a5baa91a02bc8643cbecc09ac02e27
hrallil <mathias.larsen0@gmail.com>
2022-04-19 13:56:24 +0200

    borderPane is not right aligned

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import org.sqlite.Function.Window;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,6 +7,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.Group;
@@ -31,41 +31,62 @@ public class Main extends Application {
     }
 
     public void start(Stage stage) throws Exception {
-        windowSize = 250;
         Group root = new Group(); // the root is Group or Pane
+        BorderPane bPane = new BorderPane();
+        
+        //text fields for inputting
         VBox textFields = new VBox();
-        HBox buttons = new HBox();
-
-
+        HBox courseHbox = new HBox();
+        HBox dayHbox = new HBox();
+        HBox professorHbox = new HBox();
+        HBox roomHbox = new HBox();
+        
+        // Buttons
+        VBox buttons = new VBox();
+        
         //professor text field
-        professor = new TextField("professor");
-        textFields.getChildren().add(professor);
-
+        professor = new TextField();
+        Text professorText = new Text("Professor: ");
+        professorText.prefWidth(50);
+        professorHbox.getChildren().addAll(professorText, professor);
+        textFields.getChildren().addAll(professorHbox);
+        
+        
         //day text field
         day = new TextField();
-        textFields.getChildren().add(day);
-
+        Text dayText = new Text("Day: ");
+        dayText.prefWidth(50);
+        dayHbox.getChildren().addAll(dayText, day);
+        dayHbox.setSpacing(28);
+        textFields.getChildren().addAll(dayHbox);
+        
         //room text field
         room = new TextField();
-        textFields.getChildren().add(room);
-
+        roomHbox.getChildren().addAll(new Text("room: "), room);
+        roomHbox.setSpacing(20);
+        textFields.getChildren().add(roomHbox);
+        
         // course text field
         course = new TextField();
-        textFields.getChildren().add(course);
-
+        courseHbox.getChildren().addAll(new Text("course: "), course);
+        courseHbox.setSpacing(14);
+        textFields.getChildren().add(courseHbox);
+        
         //search button
         searchButton = new Button();
         searchButton.setText("Search");
+        searchButton.setPrefHeight(75);
+        searchButton.setPrefWidth(100);
         searchButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 final Stage calander = new Stage();
                 calander.initModality(Modality.NONE);
                 calander.initOwner(stage);
-
-                VBox calanderVbox = new VBox(40);
+                
+                VBox calanderVbox = new VBox();
                 calanderVbox.getChildren().add(new Text("This will contain a calander"));
-
+                
                 Scene dialogScene = new Scene(calanderVbox, 300, 200);
                 calander.setScene(dialogScene);
                 calander.show();
@@ -77,6 +98,8 @@ public class Main extends Application {
         //update button
         updateButton = new Button();
         updateButton.setText("Update");
+        updateButton.setPrefHeight(75);
+        updateButton.setPrefWidth(100);
         updateButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -84,11 +107,16 @@ public class Main extends Application {
             }
         });
         buttons.getChildren().add(updateButton);
-
+        buttons.setSpacing(10);
+        bPane.setRight(buttons);
+        textFields.setSpacing(20);
+        
+        //add the two boxes to the root
         root.getChildren().add(textFields);
-        root.getChildren().add(buttons);
+        root.getChildren().add(bPane);
 
-        Scene scene = new Scene(root, windowSize, windowSize, Color.rgb(71, 71, 71, 1));
+        // add the root to the scene
+        Scene scene = new Scene(root, 350, 200, Color.WHITE);
         stage.setTitle("Scedule searcher and modifyer");
         stage.setScene(scene);
         stage.show();
