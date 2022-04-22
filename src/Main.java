7a90acb0dee9f20d41d3c75be390fdca6a748c6e
hrallil <mathias.larsen0@gmail.com>
2022-04-22 13:45:29 +0200

    javaFX is gay

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -37,20 +40,22 @@ public class Main extends Application {
         HBox dayHbox = new HBox();
         HBox professorHbox = new HBox();
         HBox roomHbox = new HBox();
-        String[] weekdays = {"Weekday/Timeslot","Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-        String[] detailsNames = {"Weekday:", "Timeslot:", "Course(expected students):", "Professor(s):", "Room(s),capacities:"};
+        String[] weekdays = {" Weekday/Timeslot"," Monday", " Tuesday", " Wednesday", " Thursday", " Friday"};
+        String[] detailsNames = {" Weekday:", " Timeslot:", " Course(expected students):", " Professor(s):", " Room(s),capacities:"};
         
         // Buttons
         VBox buttons = new VBox();
         
-        //professor text field
-        professor = new TextField();
-        Text professorText = new Text("Professor: ");
-        professorText.prefWidth(50);
-        professorHbox.getChildren().addAll(professorText, professor);
-        textFields.getChildren().addAll(professorHbox);
-        
-        
+        // course text field
+        course = new TextField();
+        courseHbox.getChildren().addAll(new Text("course: "), course);
+        courseHbox.setSpacing(14);
+        ComboBox ExAmountStud = new ComboBox();
+        for (int i = 0; i < 60; i++) {
+            ExAmountStud.getItems().add(i + 1);
+        }
+        textFields.getChildren().addAll(courseHbox, ExAmountStud);
+
         //day text field
         day = new TextField();
         Text dayText = new Text("Day: ");
@@ -59,27 +64,42 @@ public class Main extends Application {
         dayHbox.setSpacing(28);
         textFields.getChildren().addAll(dayHbox);
         
+        // radio checkbox for Morning/Afternoon Courses
+        HBox toggleTime = new HBox();
+        toggleTime.getChildren().addAll(new RadioButton(" Morning"),new RadioButton(" Afternoon"));
+        toggleTime.setSpacing(10);
+        textFields.getChildren().add(toggleTime);
+        
+        //professor text field
+        professor = new TextField();
+        Text professorText = new Text("Professor: ");
+        professorText.prefWidth(50);
+        professorHbox.getChildren().addAll(professorText, professor);
+        textFields.getChildren().addAll(professorHbox);
+
         //room text field
         room = new TextField();
         roomHbox.getChildren().addAll(new Text("room: "), room);
         roomHbox.setSpacing(20);
         textFields.getChildren().add(roomHbox);
         
-        // course text field
-        course = new TextField();
-        courseHbox.getChildren().addAll(new Text("course: "), course);
-        courseHbox.setSpacing(14);
-        textFields.getChildren().add(courseHbox);
+        
         
         //search button
         searchButton = new Button();
         searchButton.setText("Search");
-        searchButton.setPrefHeight(75);
+        searchButton.setPrefHeight(72);
+        searchButton.setTranslateX(5);
         searchButton.setPrefWidth(100);
         searchButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 if (calander == null || !calander.isShowing()) { // does not work....
+                    String dayTextValue = day.getText();
+                    String courseTextValue = course.getText();
+                    String professorTextValue = professor.getText();
+                    String roomTextValue = room.getText();
+
                     Stage calander = new Stage();
                     calander.initModality(Modality.NONE);
                     calander.initOwner(stage);
@@ -111,15 +131,18 @@ public class Main extends Application {
                                         detailsnameVbox.getChildren().add(new Text(detailsNames[j]));
                                         detailsValueVbox.getChildren().add(new Text("idk, smt from SQLite"));                         
                                     }
+                                    
                                     detailsHbox.getChildren().addAll(detailsnameVbox, detailsValueVbox);
-
+                                    
                                     Scene detailsScene = new Scene(detailsHbox, 275, 100);
                                     details.setTitle("Details");
-                                    details.setScene(detailsScene);
+                                    details.setScene(detailsScene); 
                                     details.show();
 
                                 }
                             });
+
+                            
                             Button afternoonButton = new Button();
                             afternoonButton.setText("idk, SMT fetched \n from SQLite");
 
@@ -143,7 +166,6 @@ public class Main extends Application {
                                     details.setTitle("Details");
                                     details.setScene(detailsScene);
                                     details.show();
-
                                 }
                             });
                             dayCalenderVBox.getChildren().add(morningButton);
@@ -171,18 +193,24 @@ public class Main extends Application {
         
         //update button
         updateButton = new Button();
-        updateButton.setText("Update");
-        updateButton.setPrefHeight(75);
+        updateButton.setText("Add");
+        updateButton.setPrefHeight(72);
         updateButton.setPrefWidth(100);
+        updateButton.setTranslateX(5);
         updateButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("update");
+                String dayTextValue = day.getText();
+                String courseTextValue = course.getText();
+                String professorTextValue = professor.getText();
+                String roomTextValue = room.getText();
+                // send to console ()
+                System.out.println();
             }
         });
         buttons.getChildren().add(updateButton);
 
-        buttons.setSpacing(10);
+        buttons.setSpacing(57);
         textFields.setSpacing(20);
         
         
@@ -190,10 +218,12 @@ public class Main extends Application {
         
         bPane.setCenter(textFields);
         bPane.setRight(buttons);
+        bPane.setTranslateX(5);
         root.getChildren().add(bPane);
+        
 
         // add the root to the scene
-        Scene scene = new Scene(root, 310, 175, Color.WHITE);
+        Scene scene = new Scene(root, 320, 210, Color.WHITE);
         stage.setTitle("Scedule searcher and modifyer");
         stage.setScene(scene);
         stage.show();
